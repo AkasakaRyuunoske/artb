bf9416ba7406c3bfc7c2a18c7b7fbe4b385fe63d
Akasaka <65616307+AkasakaRyuunoske@users.noreply.github.com>
2023-03-15 14:51:59 +0100

    Delete download_n_open.bat

--- C/download_n_open.bat
+++
@@ -1,9 +0,0 @@
-curl -o utsu-p.png -OL https://i.ytimg.com/vi/pM8Wp0Ybc88/maxresdefault.jpg
-
-curl -o message.mp3 -OL https://37s-a.musify.club/get/68/5446221/16345386/0/1/0/utsup-p-the-dying-message.mp3
-
-powershell -c %cd%\utsu-p.png
-
-powershell -c %cd%\message.mp3
-
-pause
