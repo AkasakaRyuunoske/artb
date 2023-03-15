669918ae6d250d69d236c3cf61b3f2d149974cf9
Akasaka <65616307+AkasakaRyuunoske@users.noreply.github.com>
2023-03-15 14:47:06 +0100

    Add files via upload

---
+++ C/Lezioni/Vettore Manipulations/vettore.h
@@ -0,0 +1,18 @@
+// #ifdef VETTORE_H_
+
+// #define VETTORE_H_
+
+// I/O
+void inputArray(int [], int);
+void printArray(int [], int);
+
+// Operazioni sui singoli elementi
+int  indexOf(int [], int, int);
+void delete(int [], int *, int);
+void add(int [], int *, int, int);
+
+int min(int [], int);
+
+void ordina_array(int [], int);
+
+// #endif // VETTORE_H_
