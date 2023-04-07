28a19346fd33841605d68d2ee84baa20e37fc347
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-04-07 18:02:47 +0200

    New Class added

---
+++ FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/Constants/CafeConstant.java
@@ -0,0 +1,7 @@
+package com.maid.cafe.c.maidcafec.Constants;
+
+public class CafeConstant {
+
+    public static final String YUME = "Nee anata wa itsumo yume wo mite masu ka?";
+    public static final String INVALID_DATA = "OWARI KAKE NO KONO SEKAI DE";
+}
