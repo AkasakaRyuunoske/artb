776da34a7115ca9256fd43849f8d554446ef6d6d
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-04-07 18:03:17 +0200

    New Interface added

---
+++ FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/REST/UserRest.java
@@ -0,0 +1,14 @@
+package com.maid.cafe.c.maidcafec.REST;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@RequestMapping(path = "/user")
+public interface UserRest {
+    @PostMapping(path = "/signUp")
+    public ResponseEntity<String> signUp(@RequestBody(required = true) Map<String, String> requestMap);
+}
