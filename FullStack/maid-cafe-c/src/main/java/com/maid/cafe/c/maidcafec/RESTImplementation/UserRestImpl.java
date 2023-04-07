b5f5033d47b123d11e46d901945d9e27bbc0f299
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-04-07 18:03:00 +0200

    New Class added

---
+++ FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/RESTImplementation/UserRestImpl.java
@@ -0,0 +1,33 @@
+package com.maid.cafe.c.maidcafec.RESTImplementation;
+
+import com.maid.cafe.c.maidcafec.Constants.CafeConstant;
+import com.maid.cafe.c.maidcafec.REST.UserRest;
+import com.maid.cafe.c.maidcafec.Service.UserService;
+import com.maid.cafe.c.maidcafec.Utils.CafeUtils;
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class UserRestImpl implements UserRest {
+    @Autowired
+    UserService userService;
+
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+        try{
+
+            return userService.signUp(requestMap);
+
+        } catch (Exception exception){
+            exception.printStackTrace();
+        }
+
+        // Todo return something meaningful
+        return CafeUtils.getResponseEntity(CafeConstant.YUME, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
