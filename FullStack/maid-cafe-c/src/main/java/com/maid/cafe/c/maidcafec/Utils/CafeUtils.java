7f6cbb8724ca985865f432445276eb5925cd43c9
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-04-07 18:02:51 +0200

    New Class added

---
+++ FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/Utils/CafeUtils.java
@@ -0,0 +1,12 @@
+package com.maid.cafe.c.maidcafec.Utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class CafeUtils {
+    private CafeUtils(){ }
+
+    public static ResponseEntity<String> getResponseEntity(String responseMessage, HttpStatus httpStatus){
+        return new ResponseEntity<String>("{\"message\":\"" + responseMessage + "\"", httpStatus);
+    }
+}
