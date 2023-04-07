84a66f18a2c0155f21f3f37b16f4c791635fed06
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-04-07 18:03:20 +0200

    New Interface added

---
+++ FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/Service/UserService.java
@@ -0,0 +1,9 @@
+package com.maid.cafe.c.maidcafec.Service;
+
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+public interface UserService {
+    ResponseEntity<String> signUp(Map<String, String> requestMap);
+}
