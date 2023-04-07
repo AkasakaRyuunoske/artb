de60aa313d7163330a2b27a747a977922382f467
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-04-07 18:02:23 +0200

    Initial commit

---
+++ FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/MaidCafeCApplication.java
@@ -0,0 +1,13 @@
+package com.maid.cafe.c.maidcafec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaidCafeCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaidCafeCApplication.class, args);
+	}
+
+}
---
+++ FullStack/maid-cafe-c/src/test/java/com/maid/cafe/c/maidcafec/MaidCafeCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maid.cafe.c.maidcafec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaidCafeCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
