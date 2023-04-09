5cfeb5418a7179397c4794f1db47d99fc6a10bef
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-04-09 17:19:55 +0200

    NamedQuery added

--- FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/Service/UserService.java
+++ FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/Service/UserService.java
@@ -1,11 +1,15 @@
 package com.maid.cafe.c.maidcafec.Service;
 
+import com.maid.cafe.c.maidcafec.Wrapper.UserWrapper;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
 import java.util.Map;
 
 public interface UserService {
     ResponseEntity<String> signUp(Map<String, String> requestMap);
     ResponseEntity<String> login(Map<String, String> requestMap);
+
+    ResponseEntity<List<UserWrapper>> getAllUser();
 
 }
