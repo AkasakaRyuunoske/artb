dcc4a38589373977035bbb5fcac9ddd9c1580b19
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-04-09 17:19:35 +0200

    new endpoint added

--- FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/REST/UserRest.java
+++ FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/REST/UserRest.java
@@ -1,10 +1,13 @@
 package com.maid.cafe.c.maidcafec.REST;
 
+import com.maid.cafe.c.maidcafec.Wrapper.UserWrapper;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Map;
 
 @RequestMapping(path = "/user")
@@ -14,4 +17,7 @@ public interface UserRest {
 
     @PostMapping(path = "/login")
     public ResponseEntity<String> login(@RequestBody(required = true) Map<String, String> requestMap);
+
+    @GetMapping(path = "/get")
+    public ResponseEntity<List<UserWrapper>> getAllUser();
 }
