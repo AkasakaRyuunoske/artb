7618a1443436cc5c9292ee2da92e28cb5cfef419
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-04-09 17:43:54 +0200

    NamedQuery added

--- FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/DAO/UserDAO.java
+++ FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/DAO/UserDAO.java
@@ -3,11 +3,17 @@ package com.maid.cafe.c.maidcafec.DAO;
 import com.maid.cafe.c.maidcafec.POJO.User;
 import com.maid.cafe.c.maidcafec.Wrapper.UserWrapper;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface UserDAO extends JpaRepository<User, Integer> {
     User findByEmailId(@Param("email") String email);
     List<UserWrapper> getAllUser();
+
+    @Transactional
+    @Modifying
+    Integer updateStatus(@Param("status") String status, @Param("id") Integer id);
 }
