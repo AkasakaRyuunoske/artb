6d8edff5616c0033d3f0faa28e4779ac46817209
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-04-09 17:19:50 +0200

    NamedQuery added

--- FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/RESTImplementation/UserRestImpl.java
+++ FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/RESTImplementation/UserRestImpl.java
@@ -4,12 +4,15 @@ import com.maid.cafe.c.maidcafec.Constants.CafeConstant;
 import com.maid.cafe.c.maidcafec.REST.UserRest;
 import com.maid.cafe.c.maidcafec.Service.UserService;
 import com.maid.cafe.c.maidcafec.Utils.CafeUtils;
+import com.maid.cafe.c.maidcafec.Wrapper.UserWrapper;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -41,5 +44,15 @@ public class UserRestImpl implements UserRest {
 
         // Todo return something meaningful
         return CafeUtils.getResponseEntity(CafeConstant.YUME, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List<UserWrapper>> getAllUser() {
+        try{
+            return userService.getAllUser();
+        } catch (Exception exception){
+            exception.printStackTrace();
+        } //Todo return something meaningful
+        return new ResponseEntity<List<UserWrapper>>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
