ebfa50325fb2fda8e72257cfe6a4eb10c31f6d03
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-04-09 13:03:02 +0200

    cringy line added;

--- FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/MaidCafeCApplication.java
+++ FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/MaidCafeCApplication.java
@@ -8,6 +8,7 @@ public class MaidCafeCApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaidCafeCApplication.class, args);
+		System.out.println("\n \n \"In programming, as in life, there are always obstacles to overcome. Bugs and glitches can leave us frustrated and discouraged, while setbacks and failures can leave us feeling defeated. But just as a skilled programmer must persevere through these challenges, so too must we persevere through the trials and tribulations of our lives. And in the end, it is often these struggles that make us stronger, more resilient, and more capable of creating the kind of love and life that we truly desire.\" \n ChatGPT 09/04/2023\n ");
 	}
 
 }
