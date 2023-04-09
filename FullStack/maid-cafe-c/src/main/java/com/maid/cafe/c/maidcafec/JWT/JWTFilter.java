9fa44233718666fcc71ef6461fadfe8d6d524443
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-04-09 13:02:52 +0200

    JWT Spring Security implementation and filters work perfectly fine;

--- FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/JWT/JWTFilter.java
+++ FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/JWT/JWTFilter.java
@@ -1,11 +1,18 @@
 package com.maid.cafe.c.maidcafec.JWT;
 
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,8 +22,55 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JWTFilter extends OncePerRequestFilter {
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    Claims claims;
+    private String userName;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getServletPath().matches("/user/signUp|/user/forgotPassword|/user/login")) { // Ignored urls
+            filterChain.doFilter(request, response);
+            return;
+        } else {
+            String authorizationHeader = request.getHeader("Authorization");
+            String token = null;
 
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+                token = authorizationHeader.substring(7);
+                userName = jwtUtils.extractUserName(token);
+                claims = jwtUtils.exctractAllClaims(token);
+            }
+
+            if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null){
+                UserDetails userDetailsService = customUserDetailsService.loadUserByUsername(userName);
+                if (jwtUtils.isTokenValid(token, userDetailsService)){
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetailsService,
+                            null,
+                            userDetailsService.getAuthorities());
+                    usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
+
+            filterChain.doFilter(request, response);
+
+        }
+    }
+
+    public boolean isAdmin(){
+        return "admin".equalsIgnoreCase((String) claims.get("role"));
+    }
+
+    public boolean isUser(){
+        return "user".equalsIgnoreCase((String) claims.get("role"));
+    }
+
+    public String getCurrentUser(){
+        return userName;
     }
 }
--- FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/JWT/SecurityConfig.java
+++ FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/JWT/SecurityConfig.java
@@ -1,16 +1,23 @@
 package com.maid.cafe.c.maidcafec.JWT;
 
+import com.maid.cafe.c.maidcafec.POJO.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    JWTFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
@@ -26,7 +33,9 @@ public class SecurityConfig {
                 .exceptionHandling()
                 .and()
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
