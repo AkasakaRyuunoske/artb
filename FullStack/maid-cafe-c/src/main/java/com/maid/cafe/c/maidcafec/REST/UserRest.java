0b65d5229ce93ab9635384a1fbdf8f12a2aa420f
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-04-10 12:31:14 +0200

    some more end-point added

--- FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/REST/UserRest.java
+++ FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/REST/UserRest.java
@@ -13,14 +13,23 @@ import java.util.Map;
 @RequestMapping(path = "/user")
 public interface UserRest {
     @PostMapping(path = "/signUp")
-    public ResponseEntity<String> signUp(@RequestBody(required = true) Map<String, String> requestMap);
+    ResponseEntity<String> signUp(@RequestBody(required = true) Map<String, String> requestMap);
 
     @PostMapping(path = "/login")
-    public ResponseEntity<String> login(@RequestBody(required = true) Map<String, String> requestMap);
+    ResponseEntity<String> login(@RequestBody(required = true) Map<String, String> requestMap);
 
     @GetMapping(path = "/get")
-    public ResponseEntity<List<UserWrapper>> getAllUser();
+    ResponseEntity<List<UserWrapper>> getAllUser();
 
     @PostMapping(path = "update")
-    public ResponseEntity<String> update(@RequestBody(required = true) Map<String, String> requestMap);
+    ResponseEntity<String> update(@RequestBody(required = true) Map<String, String> requestMap);
+
+    @GetMapping(path = "/checkToken")
+    ResponseEntity<String> checkToken();
+
+    @PostMapping(path = "/changePassword")
+    ResponseEntity<String> changePassword(@RequestBody Map<String, String> requestMap);
+
+    @PostMapping(path = "/forgotPassword")
+    ResponseEntity<String> forgotPassword(@RequestBody Map<String, String> requestMap);
 }
