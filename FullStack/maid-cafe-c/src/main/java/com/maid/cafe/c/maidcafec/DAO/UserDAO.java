41fa84047d7d4f8fdd221bc681e40557b49135a3
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-04-10 12:30:50 +0200

    NamedQuery added to get all admin users

--- FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/DAO/UserDAO.java
+++ FullStack/maid-cafe-c/src/main/java/com/maid/cafe/c/maidcafec/DAO/UserDAO.java
@@ -13,7 +13,11 @@ public interface UserDAO extends JpaRepository<User, Integer> {
     User findByEmailId(@Param("email") String email);
     List<UserWrapper> getAllUser();
 
+    List<String> getAllAdmin();
+
     @Transactional
     @Modifying
     Integer updateStatus(@Param("status") String status, @Param("id") Integer id);
+
+    User findByEmail(String email);
 }
