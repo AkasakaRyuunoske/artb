568569dd888cf2742d2095c8a56f5e29e7de933f
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-05-26 19:47:24 +0200

    Attempts to connect cassandra and Spring Boot (Successful this time)

--- Java/kafka simple demo/src/main/java/com/example/demo/AnaataRepository.java
+++ Java/kafka simple demo/src/main/java/com/example/demo/AnaataRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnaataRepository extends CassandraRepository<Anaata, String> {
-
+    @AllowFiltering
+    Anaata findByYume(String anaata);
 }
